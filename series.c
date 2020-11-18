86087f253b4e69a65f6c217b68998575b7a20223
Matvey <70555513+LUX23@users.noreply.github.com>
2020-11-18 23:19:15 +0700

    Update series.c

--- series.c
+++ series.c
@@ -2,18 +2,13 @@
 
 double series(double x, double eps)
 {
-    double a = -(1 / x * x), k = 1, sum = a, x = x * x, p = pow(-1, k), q = pow(-1, k + 1);
-    printf("\n\n\ta=%.2lf, k=%.2lf, sum=%.2lf, eps=%.6lf", a, k - 1, sum, eps);
-
-    while(fabs(a) > eps)
+    double a= -(1 / x * x), k = 1, sum = a;
+    x = x * x;
+    while(fabs(a)>eps)
     {
-        a = a * (q * x) / (4 * k * k * p + 2 * k * p);
-        sum = sum + a;
-        k = k + 1;
-        printf("\n\ta=%.2lf, abs(a)=%.10lf  k=%.2lf, sum=%.2lf, eps=%.6lf", a, fabs(a), k - 1, sum, eps);
+        a = a*(pow(-1, k + 1) * x) / (4 * k * k * pow(-1, k) + 2 * k * pow(-1, k));
+        sum = sum+a;
+        k = k+1;
     }
-
-    printf("\n\n\teps=%.6lf, sum=%.8lf", eps, sum);
-
-    return sum;
+    return sum + eps;
 }
