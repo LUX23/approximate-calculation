7e5317c41be713c572f2d29af5aba5490e476bd5
Matvey <70555513+LUX23@users.noreply.github.com>
2020-11-20 15:25:11 +0700

    calculation part

--- series.c
+++ series.c
@@ -1,14 +1,19 @@
 #include <math.h>
 
-double series(double x, double eps)
+double series(double x, double eps) // x - аргумент функции
+                                    // eps - доп. погрешность
 {
-    double a= -(1 / x * x), k = 1, sum = a;
+    double a= -(1 / x * x), k = 1, sum = a; // a - значение первого слагаемого ряда Тейлора
+                                            // k - индекс эл-та
+                                            // sum - тек. сумма
     x = x * x;
+
     while(fabs(a)>eps)
     {
-        a = a*(pow(-1, k + 1) * x) / (4 * k * k * pow(-1, k) + 2 * k * pow(-1, k));
-        sum = sum+a;
-        k = k+1;
+        a = a * (pow(-1, k + 1) * x) / (4 * k * k * pow(-1, k) + 2 * k * pow(-1, k)); // рекуррентная последовательность слагаемых заданного ряда
+        sum = sum + a;
+        k = k + 1;
     }
-    return sum + eps;
+
+    return sum;
 }
