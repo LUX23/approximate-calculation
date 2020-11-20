9e9c75b577c8cf8bc8856d0ced5ced880bc4818a
Matvey <70555513+LUX23@users.noreply.github.com>
2020-11-20 15:24:08 +0700

    interface part

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// Написать функцию, возвращающую значение вычисленное с заданной точностью eps.
+// Функцию протестировать в интервале [A, B] с шагом r.
+// Значения A, B, r, eps в программу вводить с клавиатуры.
+// Результаты вычислений выводить в виде таблицы.
 #include <stdio.h>
 #include <conio.h>
 #include <locale.h>
@@ -6,21 +10,26 @@ double series(double, double);
 int main()
 {
     char *locale = setlocale(LC_ALL, "RUSSIAN");
+
     double A, B;
     printf("\n\tПечатается таблица функции, заданной рядом Тейлора\n"
            "\n\tВведите границы интервала\n");
+
     do
     {
         printf("\n\t\tЛевая:   ");
         scanf("%lf", &A);
         printf("\t\tПравая:  ");
-        scanf("%lf",&B);
+        scanf("%lf", &B);
         if(A > B)
             printf("\n\t\tЛевая должна быть меньше правой! Повторите.\n");
     }
+
     while(A > B);
     double r, eps;
+
     printf("\n\n\tВведите шаг табулирования: ");
+
     do
     {
         scanf("%lf", &r);
@@ -28,25 +37,31 @@ int main()
             printf("\n\t\tШаг должен быть меньше длины интервала!"
                          "\n\t\tПовторите: ");
     }
+
     while(B - A < r);
     printf("\n\tВведите допустимую погрешность вычисления"
            "\n\n\t\t0 < eps < 1: ");
+
     do
     {
         scanf("%lf", &eps);
         if(eps >= 1 || eps <= 0) printf("\n\tВведено недопустимое значение."
                                     "\n\tПовторите: ");
     }
+
     while(eps >= 1 || eps <= 0);
     printf("\n\n\t\t\tТаблица"
            "\n\n\t\t+--------+------------+"
              "\n\t\t|   x    |    F(x)    |"
              "\n\t\t+--------+------------+");
+
     for(double x = A; x <= B + eps; x += r)
     {
         printf("\n\t\t|%8.2lf|%12.4lf|", x, series(x, eps));
         printf("\n\t\t+--------+------------+");
     }
+
     getch();
     return 0;
 }
+
